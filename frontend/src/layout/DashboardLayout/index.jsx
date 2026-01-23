219a40f5f53e5684ae8b583ab343eb2c3f9be805
Kuwar Raman Singh <singhkuwarraman@gmail.com>
2026-01-23 10:48:48 +0530

    Solved error of search icon in mobile view

--- frontend/src/layout/DashboardLayout/index.jsx
+++ frontend/src/layout/DashboardLayout/index.jsx
@@ -102,7 +102,7 @@ export default function DashboardLayout({ children }) {
 
         <div onClick={() => {
 
-          router.push("/dashboard")
+          router.push("/discover")
 
         }} className={styles.singleNavItemHolder_mobileView}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
